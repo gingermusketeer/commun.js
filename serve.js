5162748655c2aba7e318bcad25eb559d582a388a
Max Brosnahan <maximilianbrosnahan@gmail.com>
2013-03-23 23:10:20 +0000

    Update the file serving script
    Add logging for requests. Indicate the bound port. The previous version
    did not work when node was updated. So the offending code was removed.
    This has not been checked for backwards compatiblilty.

--- serve.js
+++ serve.js
@@ -1,10 +1,12 @@
 var static =  require('node-static');
-
+var PORT = 8080;
 
 var fileServer = new static.Server('./', { cache: 1});
 
-require('http').createServer(function (request, response) {
-      request.addListener('end', function () {
-                fileServer.serve(request, response);
-                    });
-}).listen(8080);
+var server = require('http').createServer(function (request, response) {
+    console.log("[" + request.method + "] " + request.url);
+
+    fileServer.serve(request, response);
+}).listen(PORT);
+
+console.log("[SERVER] - Listening on :" + PORT);
